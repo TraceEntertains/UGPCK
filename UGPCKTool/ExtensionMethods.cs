93527554f9c718f3ebffb028d8030eb49d50f879
Adam Woodruff <atw3710@gmail.com>
2022-07-22 23:04:38 -0400

    why do i ALWAYS forget the git add

---
+++ UGPCKTool/ExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UGPCKTool
+{
+    public static class ExtensionMethods
+    {
+        public static byte[] FromBase64Bytes(this byte[] base64Bytes)
+        {
+            string base64String = Encoding.UTF8.GetString(base64Bytes, 0, base64Bytes.Length);
+            return Convert.FromBase64String(base64String);
+        }
+
+        public static byte[] ToBase64Bytes(this byte[] base64Bytes)
+        {
+            return Encoding.UTF8.GetBytes(Convert.ToBase64String(base64Bytes));
+        }
+    }
+}
